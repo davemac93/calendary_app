71c5b7b0001118c6c87d0bc684e3a8782196759f
Dawid Maciejewski <dawid.maciejewski@danfoss.com>
2026-03-10 22:29:22 +0100

    Added passwaord hashing, change the users endpoint, added function which check if user exist in database

--- backend/app/api/auth.py
+++ backend/app/api/auth.py
@@ -4,20 +4,21 @@ from sqlalchemy.orm import Session
 from typing import List
 
 
-from sqlalchemy.testing.pickleable import User
-
-from ..schemas import RegisterUser, LoginUser
+from ..schemas import RegisterUser, LoginUser, RespondUser
 from ..models import User
-from ..core import get_db, create_access_token, verify_token, authenticate_user
+from ..core import get_db, create_access_token, verify_token, authenticate_user, verify_password, get_password_hash, check_user_exists
+from ..core import settings
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="api/auth/login")
 router = APIRouter(prefix="/api/auth", tags=["auth"])
 
 @router.post("/register")
 async def create_user(user_data: RegisterUser, db: Session = Depends(get_db)):
+    if check_user_exists(db, user_data.username):
+        raise HTTPException(status_code=400, detail="Username already exists")
     user = User(
         username=user_data.username,
-        password=user_data.password
+        password=get_password_hash(user_data.password)
     )
     db.add(user)
     db.commit()
@@ -28,11 +29,11 @@ async def create_user(user_data: RegisterUser, db: Session = Depends(get_db)):
 async def login(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
     user = authenticate_user(db, form_data.username, form_data.password)
     if not user:
-        raise HTTPException(status_code=400, detail="Invalid credentials")
-    access_token = create_access_token(data={"user_id": user.id})
+        raise HTTPException(status_code=401, detail="Invalid credentials")
+    access_token = create_access_token(data={"sub": str(user.id)})
     return {"access_token": access_token, "token_type": "bearer"}
 
-@router.get("/users", response_model=List[RegisterUser])
+@router.get("/users", response_model=List[RespondUser])
 async def get_users(db: Session = Depends(get_db)):
     users = db.query(User).all()
     return users
--- backend/app/models/auth.py
+++ backend/app/models/auth.py
@@ -6,7 +6,7 @@ from ..core import Base
 class User(Base):
     __tablename__ = "user"
     id = Column(Integer, primary_key=True)
-    username = Column(String)
+    username = Column(String, unique=True)
     password = Column(String)
 
 
